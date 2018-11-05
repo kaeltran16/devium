8f86dd4bbb9d3d6ad0d9f37017c27ffb4bdc2df8
Kael <piskael03@gmail.com>
2018-11-05 15:41:16 -0500

    initial

---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill/noConflict';
+import server from './server';
+
+const port = process.env.PORT || 4000;
+
+server.start({ port }, () => {
+    console.log(`🚀 Server ready at ${port}`);
+});
---
+++ server/src/resolvers/index.js
@@ -0,0 +1,14 @@
+import { extractFragmentReplacements } from 'prisma-binding';
+import Mutation from './Mutation';
+import Query from './Query';
+import User from './User';
+
+const resolvers = {
+    Query,
+    Mutation,
+    User
+};
+
+const fragmentReplacements = extractFragmentReplacements(resolvers);
+
+export { resolvers, fragmentReplacements };
