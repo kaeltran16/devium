8d645e7406024b2f14728353ea14d880ce185a71
Kael <piskael03@gmail.com>
2018-11-06 00:51:38 -0500

    setup apollo client and clean up boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+const App = () => (
+   <BrowserRouter>
+      <Switch>
+      </Switch>
+   </BrowserRouter>
+);
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,49 @@
+import ApolloClient from 'apollo-boost';
 import React from 'react';
+import { ApolloProvider, Query } from 'react-apollo';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import { currentUser } from './queries';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+   uri: 'http://localhost:5000',
+   fetchOptions: {
+      credentials: 'include'
+   },
+   request: async (operation) => {
+      const token = await localStorage.getItem('token');
+      operation.setContext({
+         headers: {
+            authorization: `Bearer ${token}`
+         }
+      });
+   },
+   onError: ({ networkError }) => {
+      if (networkError) {
+         if (networkError.statusCode === 401) {
+            localStorage.removeItem('token');
+         }
+      }
+   },
+   clientState: {
+      defaults: {}
+   }
+});
+
+const Root = () => (
+   <Query query={currentUser}>
+      {({ data, loading }) =>
+         loading ? <App/> : null
+      }
+   </Query>
+);
+
+
+ReactDOM.render(
+   <ApolloProvider client={client}>
+      <Root/>
+   </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
