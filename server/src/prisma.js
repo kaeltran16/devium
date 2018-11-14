6153f7476e8a5c4b7acd62e0b51728c128493419
kael <piskael03@gmail.com>
2018-11-14 11:01:44 -0500

    fix token bug in prisma server

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,11 +1,10 @@
 import { Prisma } from 'prisma-binding';
 
-console.log(process.env.PRISMA_ENDPOINT);
-
 const prisma = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
-    endpoint: process.env.PRISMA_ENDPOINT,
-    secret: process.env.PRISMA_SECRET
+   typeDefs: 'src/generated/prisma.graphql',
+   endpoint: process.env.PRISMA_ENDPOINT,
+   secret: process.env.PRISMA_SECRET,
+   debug: true
 });
 
 
